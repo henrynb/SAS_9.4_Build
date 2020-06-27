9bcfa0a9537ac571e98a3e3b554af3c330475ca9
henrynb <henry.newman-burke@butterflydata.co.uk>
2020-06-27 17:05:31 +0100

    Ammendments

--- scripts/first_thing.sh
+++ scripts/first_thing.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 # First thing to do
 
-echo "Install X11"
-
-# Install X11 assumes the registration
-yum install xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps -y
-
 echo "Install Pre Reqs"
 
-# Install libXp
-yum install libXp libXmu numactl mailx java-1.8.0-openjdk-devel lsof -y
+# Install X11 assumes the registration
+yum install xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps libXp libXmu numactl mailx java-1.8.0-openjdk-devel lsof tree tmux -y
 
 # check the openssl
 echo "verify openssl"
 
 openssl s_client -connect linuxconfig.org:443 -showcerts
 
-# create the root folder for SAS
+# create directories
+
+# create the root directory for SAS
 mkdir /opt/sas
+
+# create the sub-directories within the root directory for SAS
+mkdir -p /opt/sas/sashome/
+mkdir -p /opt/sas/sasconfig/
+mkdir -p /opt/sas/saswork/
+mkdir -p /opt/sas/sasbackup/
+mkdir -p /opt/sas/sashfadd/
+mkdir -p /opt/sas/junit/
+
+#create the sub-directory for the sasdepot mount
+mkdir -p /mnt/sasdepot/
+
+# create the vmware vfabric folder
+mkdir -p /etc/opt/vmware/vfabric/
+
+#mount the sasdepot
+mount /dev/xvdf1 /mnt/sasdepot/
